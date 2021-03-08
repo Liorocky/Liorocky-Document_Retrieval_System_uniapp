88a8446aa91be5af27f194806455869f4657af38
Lior <R.Donghui@outlook.com>
2021-03-08 22:50:38 +0800

    页面初始布局

---
+++ Document_Retrieval_System_uniapp/pages/retrieve/retrieve.vue
@@ -0,0 +1,39 @@
+<template>
+	<view>
+		<view>
+			<uni-search-bar :radius="100" @confirm="search" @input="input" ></uni-search-bar>			
+		</view>
+		<view>
+			<text>常用标签</text>
+			<view class="tags">
+				<uni-tag text="标签"></uni-tag>
+				<uni-tag text="标签" type="error" :circle="true"></uni-tag>
+				<uni-tag text="标签" @click="bindClick"></uni-tag>
+			</view>
+			
+		</view>
+		
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.tags{
+	display: flex;
+	justify-content: space-between;
+	align-items:center;
+}
+</style>
---
+++ Document_Retrieval_System_uniapp/pages/upload/upload.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		录入文档
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
