087eadb225e1baccdcd1408992b1486f3337bdf5
Lior <R.Donghui@outlook.com>
2021-03-23 00:18:46 +0800

    引入uview

--- Document_Retrieval_System_uniapp/pages/page-box-detail/page-box-detail.vue
+++ Document_Retrieval_System_uniapp/pages/page-box-detail/page-box-detail.vue
@@ -1,8 +1,19 @@
 <template>
 	<view>
-		<view class="title">标题</view>
-		<input :value="fileBoxData.title" :disabled="isNotEditable"></input>
+		<view class="">
+			<u-form :model="fileBoxData" ref="uForm">
+				<u-form-item label="" prop="title" label-position="left" left-icon="info">
+					<u-input v-model="fileBoxData.title" disabled/>
+				</u-form-item>
+				<u-form-item label="" prop="desc" label-position="left" left-icon="file-text">
+					<u-input v-model="fileBoxData.desc" auto-height />
+				</u-form-item>
+			</u-form>
+		</view>
+		
+		<!-- <file-data-box></file-data-box> -->
 	</view>
+
 </template>
 
 <script>
@@ -10,19 +21,17 @@
 		onLoad: function(option) { //option为object类型，会序列化上个页面传递的参数
 			const item = JSON.parse(decodeURIComponent(option.item));
 			// uni.$once('clickBox', this.setFileBoxData)
-			console.log("item: ",item);
+			console.log("item: ", item);
 			this.fileBoxData = item
 		},
 		data() {
 			return {
 				fileBoxData: {},
-				isNotEditable: true // 是否可编辑
+				isNotEditable: true // 是否不可编辑
 			}
 		},
-		beforeCreate() {
-		},
-		mounted() {
-		},
+		beforeCreate() {},
+		mounted() {},
 		methods: {
 			// setFileBoxData(data) {
 			// 	console.log(data, 'data')
